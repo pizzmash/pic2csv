5739b3be1398ecb6a3aabb128df126765207281f
Yuma Suzuki <s1813561@s.tsukuba.ac.jp>
2019-12-10 11:15:47 +0900

    テキストの合成

--- dialog_matching.py
+++ dialog_matching.py
@@ -45,7 +45,7 @@ def recall_matching(frames, frames_ref):
     matched_Fs = []
     matched_indexes = []
     for rframe in frames_ref:
-        Fs = [culc_F(frame, rframe) for frame in frames]
+        Fs = [culc_F(frame[0], rframe[0]) for frame in frames]
         max_F = max(Fs)
         max_index = Fs.index(max_F)
         matched_Fs.append(max_F if max_F != 0 else None)
@@ -68,8 +68,8 @@ def rectangles(img, frames, color, thickness=1):
     for frame in frames:
         cv2.rectangle(
             img,
-            (frame[0], frame[1]),
-            (frame[0]+frame[2], frame[1]+frame[3]),
+            (frame[0][0], frame[0][1]),
+            (frame[0][0]+frame[0][2], frame[0][1]+frame[0][3]),
             color,
             thickness=thickness
         )
@@ -92,13 +92,14 @@ def draw_recall_matched_frames(img, frames, frames_ref):
 
 
 def extract_frame_info(frame):
-    info_names = ['startX', 'startY', 'width', 'height']
-    return [int(frame[name]) for name in info_names]
+    dialog_names = ['startX', 'startY', 'width', 'height']
+    text_names = 'text'
+    return [[int(frame[name]) for name in dialog_names], frame[text_names]]
 
 
 def remove_mini(frames, min_w=0, min_h=0):
     return [
-        frame for frame in frames if frame[2] >= min_w and frame[3] >= min_h
+        frame for frame in frames if frame[0][2] >= min_w and frame[0][3] >= min_h
     ]
 
 
@@ -109,22 +110,25 @@ def remove_inclusion(frames):
             continue
         for j, fj in enumerate(frames):
             if i != j:
-                overlap = culc_overlap(fi, fj)
+                overlap = culc_overlap(fi[0], fj[0])
                 if overlap is not None:
-                    if overlap == fi:
+                    if overlap == fi[0]:
                         remove_idx.append(i)
                         break
-                    elif overlap == fj:
+                    elif overlap == fj[0]:
                         remove_idx.append(j)
     return [frame for i, frame in enumerate(frames) if i not in remove_idx]
 
 
 def expand(frame, expansion):
     return [
-        max(0, frame[0]-expansion),
-        max(0, frame[1]-expansion),
-        frame[2]+expansion*2,
-        frame[3]+expansion*2
+        [
+            max(0, frame[0][0]-expansion),
+            max(0, frame[0][1]-expansion),
+            frame[0][2]+expansion*2,
+            frame[0][3]+expansion*2
+        ],
+        frame[1]
     ]
 
 
@@ -132,7 +136,7 @@ def create_graph(frames):
     graph = [[False] * len(frames) for _ in range(len(frames))]
     for i, fi in enumerate(frames):
         for j, fj in enumerate(frames):
-            if i != j and culc_overlap(fi, fj) is not None:
+            if i != j and culc_overlap(fi[0], fj[0]) is not None:
                 graph[i][j] = True
                 graph[j][i] = True
     return graph
@@ -154,6 +158,10 @@ def divide_groups(graph):
     return groups
 
 
+def sorted_by_distance(frames):
+    return sorted(frames, key=lambda f: f[0][0]**2+f[0][1]**2, reverse=True)
+
+
 def combine_nearby(frames, expansion=3):
     expanded = [expand(frame, expansion) for frame in frames]
     graph = create_graph(expanded)
@@ -162,16 +170,19 @@ def combine_nearby(frames, expansion=3):
     result = []
     for group in groups:
         gframes = [frames[i] for i in group]
-        x = min([gf[0] for gf in gframes])
-        y = min([gf[1] for gf in gframes])
-        w = max([gf[0]+gf[2] for gf in gframes]) - x
-        h = max([gf[1]+gf[3] for gf in gframes]) - y
-        result.append([x, y, w, h])
+        x = min([gf[0][0] for gf in gframes])
+        y = min([gf[0][1] for gf in gframes])
+        w = max([gf[0][0]+gf[0][2] for gf in gframes]) - x
+        h = max([gf[0][1]+gf[0][3] for gf in gframes]) - y
+        text = ''
+        for gframe in sorted_by_distance(gframes):
+            text += gframe[1]
+        result.append([[x, y, w, h], text])
     return result
 
 
 def preprocess(frames):
-    result = remove_mini(frames, min_w=10, min_h=10)
+    result = remove_mini(frames, min_w=15, min_h=15)
     result = remove_inclusion(result)
     result = combine_nearby(result)
     return result
@@ -180,27 +191,22 @@ def preprocess(frames):
 def main():
     pages = read_csv('pages.csv')
     pages_ref = read_csv('pages_ref.csv')
-    n_frames, n_rframes = None, None
-    n_matched = 0
     for page, rpage in zip(pages, pages_ref):
         frames = read_csv('frames.csv')
         frames_ref = read_csv('frames_ref.csv')
-        """
-        if n_frames is None:
-            n_frames = len(list(frames))
-            n_rframes = len(list(frames_ref))
-        """
         img = cv2.imread(page['source'], cv2.IMREAD_COLOR)
         frames = [extract_frame_info(frame) for frame in frames if frame['page_id'] == page['page_id']]
         rframes = [extract_frame_info(rframe) for rframe in frames_ref if rframe['page_id'] == page['page_id']]
-        # origin = frames
+        origin = frames
         frames = preprocess(frames)
         draw_frames(img, frames, rframes)
-        n_matched += draw_recall_matched_frames(img, frames, rframes)
-        # rectangles(img, origin, (0, 0, 255))
+        draw_recall_matched_frames(img, frames, rframes)
+        for frame in frames:
+            print(frame[1])
+        print()
+        rectangles(img, origin, (0, 0, 255))
         cv2.imshow('result', img)
         cv2.waitKey(0)
-    print("{}, {}, {}".format(n_frames, n_rframes, n_matched))
 
 
 if __name__ == '__main__':
